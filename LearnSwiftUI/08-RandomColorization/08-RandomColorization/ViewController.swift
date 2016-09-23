dc7b5815135108374e4e7a0055803340671861cd
wangyingbo <wangyingbo528@126.com>
2016-09-23 14:12:44 +0800

    08 customRadomColor

---
+++ LearnSwiftUI/08-RandomColorization/08-RandomColorization/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  08-RandomColorization
+//
+//  Created by 王迎博 on 16/9/22.
+//  Copyright © 2016年 王迎博. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button: UIButton!
+    var audioPlayer = AVAudioPlayer()
+    let gradientLayer = CAGradientLayer()
+    
+    var timer : NSTimer?
+    
+    
+    
+    @IBAction func playMusic(sender: UIButton)
+    {
+        
+    }
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+    }
+
+
+
+}
+
