e3720d705324161a66445b639098d9e1233c2092
wangyingbo <wangyingbo528@126.com>
2016-10-13 16:23:31 +0800

    some info

--- LearnSwiftGrammar/L004Strings/L004Strings/main.swift
+++ LearnSwiftGrammar/L004Strings/L004Strings/main.swift
@@ -93,45 +93,45 @@ for code16 in unicodeString.utf16 {
 字符串函数及运算符
 Swift 支持以下几种字符串函数及运算符：
 序号	函数/运算符 & 描述
-1
-isEmpty
-判断字符串是否为空，返回布尔值
-2
-hasPrefix(prefix: String)
-检查字符串是否拥有特定前缀
-3
-hasSuffix(suffix: String)
-检查字符串是否拥有特定后缀。
-4
-Int(String)
-转换字符串数字为整型。 实例:
-let myString: String = "256"
-let myInt: Int? = Int(myString)
-5
-String.characters.count
-计算字符串的长度
-6
-utf8
-您可以通过遍历 String 的 utf8 属性来访问它的 UTF-8 编码
-7
-utf16
-您可以通过遍历 String 的 utf8 属性来访问它的 UTF-16 编码
-8
-unicodeScalars
-您可以通过遍历String值的unicodeScalars属性来访问它的 Unicode 标量编码.
-9
-    +
-连接两个字符串，并返回一个新的字符串
-10
-    +=
-连接操作符两边的字符串并将新字符串赋值给左边的操作符变量
-11
-    ==
-判断两个字符串是否相等
-12
-    <
-    比较两个字符串，对两个字符串的字母逐一比较。
-13	
-    !=
-    比较两个字符串是否不相等。
+    1
+    isEmpty
+    判断字符串是否为空，返回布尔值
+    2
+    hasPrefix(prefix: String)
+    检查字符串是否拥有特定前缀
+    3
+    hasSuffix(suffix: String)
+    检查字符串是否拥有特定后缀。
+    4
+    Int(String)
+    转换字符串数字为整型。 实例:
+    let myString: String = "256"
+    let myInt: Int? = Int(myString)
+    5
+    String.characters.count
+    计算字符串的长度
+    6
+    utf8
+    您可以通过遍历 String 的 utf8 属性来访问它的 UTF-8 编码
+    7
+    utf16
+    您可以通过遍历 String 的 utf8 属性来访问它的 UTF-16 编码
+    8
+    unicodeScalars
+    您可以通过遍历String值的unicodeScalars属性来访问它的 Unicode 标量编码.
+    9
+        +
+    连接两个字符串，并返回一个新的字符串
+    10
+        +=
+    连接操作符两边的字符串并将新字符串赋值给左边的操作符变量
+    11
+        ==
+    判断两个字符串是否相等
+    12
+        <
+        比较两个字符串，对两个字符串的字母逐一比较。
+    13	
+        !=
+        比较两个字符串是否不相等。
 */
