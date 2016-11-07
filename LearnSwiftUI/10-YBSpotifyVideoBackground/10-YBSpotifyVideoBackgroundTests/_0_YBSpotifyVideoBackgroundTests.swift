c367ffaf08573ed97e590cc79c3c40982590e186
wangyingbo <wangyingbo528@126.com>
2016-11-07 15:14:24 +0800

    10-视频引导登录页

---
+++ LearnSwiftUI/10-YBSpotifyVideoBackground/10-YBSpotifyVideoBackgroundTests/_0_YBSpotifyVideoBackgroundTests.swift
@@ -0,0 +1,36 @@
+//
+//  _0_YBSpotifyVideoBackgroundTests.swift
+//  10-YBSpotifyVideoBackgroundTests
+//
+//  Created by 王迎博 on 16/11/7.
+//  Copyright © 2016年 王迎博. All rights reserved.
+//
+
+import XCTest
+@testable import _0_YBSpotifyVideoBackground
+
+class _0_YBSpotifyVideoBackgroundTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
